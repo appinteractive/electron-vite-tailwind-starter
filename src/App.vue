c6c5a19db857275a497e00cf8c8c04c23cc9c75a
Grzegorz Leoniec <greg@app-interactive.de>
2021-02-08 15:25:10 +0100

    Upgraded to Vite 2 + logo

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,7 @@
     <!-- [ HERE COMES YOUR CONTENT ] -->
     <transition name="fade" appear>
       <div class="flex flex-col items-center justify-center h-full pb-0">
-        <img class="h-32 ml-5" src="/logo.png" draggable="false" />
-        <h1
-          class="text-3xl tracking-wider text-primary-100 font-semibold items-center justify-center flex"
-        >
-          Electron <span class="font-semibold text-base px-6">+</span> Vite
-          <span class="font-semibold text-base px-6">+</span> Tailwind
-        </h1>
+        <img class="h-48 ml-5" src="/logo.png" draggable="false" />
       </div>
     </transition>
   </div>
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
+const vue = require('@vitejs/plugin-vue')
 const path = require('path')
 const srcPath = path.resolve(__dirname, 'src')
 
 module.exports = {
-  open: false, // do not open the browser as we use electron
-  port: process.env.PORT || 3333,
+  server: {
+    open: false, // do not open the browser as we use electron
+    port: process.env.PORT || 3333,
+  },
   alias: {
     // setup aliases for cleaner imports
-    '/~/': srcPath,
+    '/~': srcPath,
   },
+  plugins: [vue()],
   optimizeDeps: {
-    // exclode path and electron-window-state as we are using the node runtime inside the browser
-    // and dont wont vite to complain. If you have any issues importing node packages and vite complains,
+    // exclude path and electron-window-state as we are using the node runtime inside the browser
+    // and don't wont vite to complain. If you have any issues importing node packages and vite complains,
     // add them here
     exclude: ['path', 'electron-window-state'],
   },
